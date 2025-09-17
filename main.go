44f796a08efbe58121164bb287c4aa82d769380a
RashidChy <sakinchy82@gmail.com>
2025-09-17 13:14:40 +0600

    majority element

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ func main() {
 
 	// problems
 
-	//fmt.Println(problems.MissingNumber([]int{0, 2}))
-	//fmt.Println(problems.TwoSum([]int{2, 7, 11, 15, 13}, 15))
-	//problems.Merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3) // merge two sorted array
-	//problems.MaxProfit2([]int{7, 1, 5, 3, 6, 4}) // best time to buy and sell stocks
-	//problems.ContainsDuplicate([]int{1, 2, 3, 1})
-	//problems.MaxProduct([]int{2, 3, -2, 4})
-	//problems.ProductExceptSelf([]int{1, 2, 3, 4})
+	// fmt.Println(problems.MissingNumber([]int{0, 2}))
+	// fmt.Println(problems.TwoSum([]int{2, 7, 11, 15, 13}, 15))
+	// problems.Merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3) // merge two sorted array
+	// problems.MaxProfit2([]int{7, 1, 5, 3, 6, 4})                  // best time to buy and sell stocks
+	// problems.ContainsDuplicate([]int{1, 2, 3, 1})
+	// problems.MaxProduct([]int{2, 3, -2, 4})
+	// problems.ProductExceptSelf([]int{1, 2, 3, 4})
 	problems.MaxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}) // kadane's Algorithm
 
 	// DSA
---
+++ problems/majority-element.go
@@ -0,0 +1,25 @@
+package problems
+
+// Mooer's Voting Algorithm :  If we pair each occurrence of the majority element with a different element, the majority element will still remain because it occurs more than half the time.
+
+// https://leetcode.com/problems/majority-element/submissions/1773605864/
+
+func MajorityElement(nums []int) int {
+	count := 0
+	var curr int
+
+	for i := 0; i < len(nums); i++ {
+		if count == 0 {
+			curr = nums[i]
+		}
+		if len(nums) == 1 {
+			return nums[0]
+		}
+		if curr == nums[i] {
+			count++
+		} else {
+			count--
+		}
+	}
+	return curr
+}
